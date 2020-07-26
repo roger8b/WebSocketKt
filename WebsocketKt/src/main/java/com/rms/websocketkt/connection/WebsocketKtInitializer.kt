0653adea575ff8e71294370538fa1d6e8533052a
Roger <roger.8b@gmail.com>
2020-07-26 02:37:21 -0300

    Create WebsocketKtInitializer.kt

---
+++ WebsocketKt/src/main/java/com/rms/websocketkt/connection/WebsocketKtInitializer.kt
@@ -0,0 +1,34 @@
+package com.rms.websocketkt.connection
+
+import com.rms.websocketkt.api.WebsocketKt
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.util.concurrent.TimeUnit
+
+data class WebSocketKtInitializer(
+    var url: String? = null,
+    var readTimeout: Long? = null
+)
+
+
+fun webSocketKtInitializer(init: WebSocketKtInitializer.() -> Unit): WebsocketKt {
+    val webSocketInitializer = WebSocketKtInitializer()
+    webSocketInitializer.init()
+
+    val url = checkNotNull(webSocketInitializer.url) {
+        "[webSocketKtInitializer] url must be set "
+    }
+    val readTimeout = checkNotNull(webSocketInitializer.readTimeout) {
+        "[webSocketKtInitializer] readTimeout must be set "
+    }
+
+    val client: OkHttpClient = OkHttpClient.Builder()
+        .readTimeout(readTimeout, TimeUnit.MILLISECONDS)
+        .build()
+    val request: Request = Request.Builder()
+        .url(url)
+        .build()
+
+    return WebsocketKtImpl(client, request)
+}
+
